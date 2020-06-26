83c76a8193cc07110cc8d5a44b1bd42b9a917b21
TiagoBehenck <tiago.behenck.santos@gmail.com>
2020-06-26 10:58:50 -0300

    feat: create a page reset password

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,24 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import ReactLoading from 'react-loading';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
   <Container type="button" {...rest}>
-    {children}
+    {loading ? (
+      <ReactLoading
+        width={16}
+        height={16}
+        type="spinningBubbles"
+        className="spinner"
+      />
+    ) : (
+      children
+    )}
   </Container>
 );
 
